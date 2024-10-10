cb8ea3880332cae8ba1102924f525a1d668d2ff5
Goodness Ezeokafor <gootech442@gmail.com>
2024-10-10 18:03:39 +0100

    chore: cleaned up HttpExceptionFilter

--- src/shared/http-exception/index.ts
+++ src/shared/http-exception/index.ts
@@ -16,22 +16,6 @@ export class HttpExceptionFilter implements ExceptionFilter {
   ERROR_MESSAGE = 'An error occurred, please contact support';
   ERROR_MESSAGES = [
     'Session has expired, please login or restart reset password process.',
-    'You do not have a Transaction PIN set up',
-    'Please enter transaction pin',
-    'You entered a wrong PIN. Your account will be blocked for 3 hours after 2 more attempts',
-    'You entered a wrong PIN. Your account will be blocked for 3 hours after 1 more attempt',
-    'You account has been blocked for 3 hours because you entered a wrong pin 3 times',
-    'jwt expired',
-    'jwt malformed',
-    'Duplicate transaction detected',
-    'BVN does not exist or is incorrect',
-    'NIN does not exist or is incorrect',
-    'Session has expired, please login',
-    'Email is not correct',
-    'You have exceeded the maximum transaction limit on your account. Kindly upgrade your account or try again after 24hours.',
-    'This account does not exist',
-    'invalid signature',
-    'Duplicate transaction detected. Try again in 5 minutes.'
   ];
   ERROR_STATUSES = [404];
 
